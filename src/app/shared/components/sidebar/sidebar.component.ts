94ea0f74436e72cfc2b7b78949d989e941bc0a45
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-04-02 14:13:05 +0200

    GifsService

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { GifsService } from '../../../gifs/services/gifs.service';
 
 @Component({
-  selector: 'shired-sidebar',
-  templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+	selector: 'shired-sidebar',
+	templateUrl: './sidebar.component.html',
+	styleUrl: './sidebar.component.css',
 })
 export class SidebarComponent {
+	constructor(private gifsService: GifsService) {}
 
+	get tags() {
+		return this.gifsService.tagsHistory;
+	}
 }
