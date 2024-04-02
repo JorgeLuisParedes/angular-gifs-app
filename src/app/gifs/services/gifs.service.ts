a6e7a29fe5210dd752732ac2af78483b18587db2
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-04-02 14:38:12 +0200

    Validaciones al servicio

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -10,7 +10,24 @@ export class GifsService {
 		return [...this._tagsHistory];
 	}
 
+	private organizeHistory(tag: string) {
+		tag = tag.toLowerCase();
+
+		if (this._tagsHistory.includes(tag)) {
+			this._tagsHistory = this._tagsHistory.filter(
+				(oldTag) => oldTag !== tag
+			);
+		}
+
+		this._tagsHistory = this.tagsHistory.slice(0, 10);
+	}
+
 	public searchTag(tag: string): void {
+		if (tag.length === 0) return;
+		this.organizeHistory(tag);
+
 		this._tagsHistory.unshift(tag);
+
+		console.log(this.tagsHistory);
 	}
 }
